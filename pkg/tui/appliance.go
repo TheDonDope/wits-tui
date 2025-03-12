1b8ecff23f0ca7c446cd81fb377e337722fefd1e
Christian Dobert <thedondope@hey.com>
2025-03-12 18:16:42 +0100

    feat(pkg-tui): add home view model

--- pkg/tui/appliance.go
+++ pkg/tui/appliance.go
@@ -1,6 +1,55 @@
 package tui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// MaxWidth defines the maximum boundaries the application has to adhere to
+const MaxWidth = 120
+
+var (
+	red    = lipgloss.AdaptiveColor{Light: "#FE5F86", Dark: "#FE5F86"}
+	indigo = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
+	green  = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
+)
+
+// Styles defines the used lipgloss styles
+type Styles struct {
+	Base,
+	HeaderText,
+	Status,
+	StatusHeader,
+	Highlight,
+	ErrorHeaderText,
+	Help lipgloss.Style
+}
+
+// NewStyles returns the new configured styles using the given renderer.
+func NewStyles(lg *lipgloss.Renderer) *Styles {
+	s := Styles{}
+	s.Base = lg.NewStyle().
+		Padding(1, 4, 0, 1)
+	s.HeaderText = lg.NewStyle().
+		Foreground(indigo).
+		Bold(true).
+		Padding(0, 1, 0, 2)
+	s.Status = lg.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(indigo).
+		PaddingLeft(1).
+		MarginTop(1)
+	s.StatusHeader = lg.NewStyle().
+		Foreground(green).
+		Bold(true)
+	s.Highlight = lg.NewStyle().
+		Foreground(lipgloss.Color("212"))
+	s.ErrorHeaderText = s.HeaderText.
+		Foreground(red)
+	s.Help = lg.NewStyle().
+		Foreground(lipgloss.Color("240"))
+	return &s
+}
 
 // HomeViewBuilder builds home views from a set of given mandatory and optional components:
 // title, list (table), list buttons, preview.
@@ -21,6 +70,10 @@ type HomeViewBuilder interface {
 
 // HomeView holds the default implementation for the HomeViewBuilder
 type HomeView struct {
+	lg     *lipgloss.Renderer
+	styles *Styles
+	width  int
+
 	title      string
 	titleLabel tea.Model
 
@@ -34,8 +87,13 @@ type HomeView struct {
 
 // NewHomeView returns a new HomeView with empty content.
 func NewHomeView() *HomeView {
-	return &HomeView{}
+	m := &HomeView{width: MaxWidth}
+	m.lg = lipgloss.DefaultRenderer()
+	m.styles = NewStyles(m.lg)
+	return m
 }
+
+// HomeViewBuilder interface ---------------------------------------------------
 
 // Title sets the title to render
 func (hv *HomeView) Title(t string) {
@@ -70,6 +128,59 @@ func (hv *HomeView) Panel() tea.Model {
 	return hv.panel
 }
 
+// tea.Model interface ---------------------------------------------------------
+
+// Init ...
+func (hv *HomeView) Init() tea.Cmd {
+	return nil
+}
+
+// Update ...
+func (hv *HomeView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return hv, tea.Interrupt
+		case "esc", "q":
+			return hv, tea.Quit
+		}
+	}
+	var cmds []tea.Cmd
+
+	return hv, tea.Batch(cmds...)
+}
+
+// View ...
+func (hv *HomeView) View() string {
+	s := hv.styles
+
+	header := hv.appBoundaryView(hv.title)
+
+	barExtras := lipgloss.JoinHorizontal(lipgloss.Top, hv.listBar.View(), hv.listExtras.View())
+
+	body := lipgloss.JoinVertical(lipgloss.Left, hv.listView.View(), barExtras, hv.preview.View())
+
+	return s.Base.Render(header + "\n" + body)
+}
+
 func (hv *HomeView) buildPanel() tea.Model {
 	return nil
+}
+
+func (hv *HomeView) appBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		hv.width,
+		lipgloss.Left,
+		hv.styles.HeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(indigo),
+	)
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
 }
