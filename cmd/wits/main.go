0016bb158f59fe0ebb37b1f2d930ae3764975dc8
Christian Dobert <thedondope@hey.com>
2025-03-12 12:07:40 +0100

    fix(cmd-wits): remove wrong ignore and re-add wits command

---
+++ cmd/wits/main.go
@@ -0,0 +1,26 @@
+// Package main is the entry point for the Wits TUI application.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/TheDonDope/wits-tui/pkg/service"
+	"github.com/TheDonDope/wits-tui/pkg/storage"
+	"github.com/TheDonDope/wits-tui/pkg/tui"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	strainStore, err := storage.NewStrainStoreYMLFile()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading data from YML File: %v", err)
+		os.Exit(1)
+	}
+	strainService := service.NewStrainService(strainStore)
+	_, err = tea.NewProgram(tui.InitialMenuModel(strainService, strainStore)).Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting program: %v", err)
+		os.Exit(1)
+	}
+}
