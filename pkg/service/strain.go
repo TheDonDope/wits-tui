dc369f01edb2d8b9382f8139db668d63c2d7cc44
Christian Dobert <thedondope@hey.com>
2025-03-16 18:22:47 +0100

    feat(pkg-service): add logging to strain service

--- pkg/service/strain.go
+++ pkg/service/strain.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"fmt"
+	"log"
+
 	can "github.com/TheDonDope/wits-tui/pkg/cannabis"
 	"github.com/TheDonDope/wits-tui/pkg/storage"
 )
@@ -19,20 +22,27 @@ type StrainServiceType struct {
 
 // NewStrainService creates a new service layer for strains.
 func NewStrainService(s storage.StrainStore) *StrainServiceType {
+	log.Println("💬 🤝  (pkg/svc/strain.go) NewStrainService(s storage.StrainStore)")
+	log.Println(fmt.Sprintf("✅ 🤝  (s): %v", s))
 	return &StrainServiceType{store: s}
 }
 
 // AddStrain adds a strain to the store.
 func (svc *StrainServiceType) AddStrain(s *can.Strain) error {
+	log.Println("💬 🤝  (pkg/svc/strain.go) AddStrain(s *can.Strain)")
+	log.Println(fmt.Sprintf("✅ 🤝  (s): %v", s))
 	return svc.store.AddStrain(s)
 }
 
 // GetStrains retrieves all strains from the store.
 func (svc *StrainServiceType) GetStrains() []*can.Strain {
+	log.Println("💬 🤝  (pkg/svc/strain.go) GetStrains()")
 	return svc.store.GetStrains()
 }
 
 // FindStrainByProduct looks up a strain by its prodcut name.
 func (svc *StrainServiceType) FindStrainByProduct(p string) (*can.Strain, error) {
+	log.Println("💬 🤝  (pkg/svc/strain.go) FindStrainByProduct(p string)")
+	log.Println(fmt.Sprintf("✅ 🤝  (p): %v", p))
 	return svc.store.FindStrainByProduct(p)
 }
